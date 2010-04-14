ad5514717acd353238d417838b293f96968da473
Thibault Saunier <tsaunier@src.gnome.org>
2010-04-13 20:19:08 -0400

    Add HolaMundo!

--- sitioPrincipal/views.py
+++ sitioPrincipal/views.py
@@ -1,1 +1,7 @@
+from django.http import HttpResponse
+
 # Create your views here.
+def HolaMundo (request):
+    html = "<html><body>Hola Mundo</body></html>"
+    return HttpResponse(html)
+
--- urls.py
+++ urls.py
@@ -16,5 +16,5 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     (r'^admin/', include(admin.site.urls)),
-    #(r'^sitioPrincipal/', include(studentManager.sitioPrincipa)),
+    (r'^HolaMundo/', 'studentManager.sitioPrincipal.views.HolaMundo'),
 )
