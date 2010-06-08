7e70251e52b7e9e1d3356681e29deb209b734f88
Thibault Saunier <tsaunier@src.gnome.org>
2010-06-08 13:48:49 -0300

    Add models

--- sitioPrincipal/models.py
+++ sitioPrincipal/models.py
@@ -1,7 +1,94 @@
 from django.db import models
 
-# Create your models here.
-class Person(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
+class Ramo(models.Model):
+    pass
 
+class Profesor(models.Model):
+    pass
+
+class Promocion(models.Model):
+    pass
+
+class Alumno(models.Model):
+    pass
+
+class Ano(models.Model):
+    pass
+
+class Carrera(models.Model):
+    nombre = models.CharField(max_length=100)
+
+    #Relaciones
+    ramos = models.ManyToManyField(Ramo)
+    jefeCarrera = models.OneToOneField(Profesor)
+
+# Create your models here.
+class Profesor(models.Model):
+    rut = models.CharField(max_length=30, primary_key=True)
+    apelido = models.CharField(max_length=100)
+    nombre = models.CharField(max_length=100)
+
+    #Relaciones
+    jefeCarrera = models.OneToOneField(Carrera)
+
+class Alumno(models.Model):
+    rut = models.CharField(max_length=30, primary_key=True)
+    apelido = models.CharField(max_length=100)
+    nombre = models.CharField(max_length=100)
+    telefono = models.CharField(max_length=100)
+    telefono_casa = models.CharField(max_length=100)
+    direccion = models.CharField(max_length=100)
+    direccion_casa = models.CharField(max_length=100)
+    estado = models.CharField(max_length=100)
+
+    #Relaciones
+    carrera = models.ForeignKey(Carrera)
+    promocion = models.ForeignKey(Promocion)
+    solicitud = models.ManyToManyField(Alumno, through='Solicitud')
+
+class Ramo(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.CharField(max_length=500)
+
+    #Relaciones
+    profesor = models.ForeignKey(Profesor)
+    estudiantes = models.ManyToManyField(Alumno)
+
+class Carrera(models.Model):
+    nombre = models.CharField(max_length=100)
+
+    #Relaciones
+    ramos = models.ManyToManyField(Ramo)
+    jefeCarrera = models.OneToOneField(Profesor)
+
+class Promocion(models.Model):
+    #Relaciones
+    ano = models.ForeignKey(Ano)
+
+class Ano(models.Model):
+    ano = models.IntegerField(primary_key=True)
+    #Relaciones
+
+class Asignatura(models.Model):
+    seccion = models.IntegerField()
+    periodo =  models.IntegerField()
+
+    #Relaciones
+    carrera = models.ForeignKey(Carrera)
+    alumnos = models.ManyToManyField(Alumno, through='Prueba')
+
+class Prueba(models.Model):
+    titulo = models.CharField(max_length=100)
+    nota = models.FloatField()
+    date = models.DateField()
+
+    #Relaciones
+    alumno = models.ForeignKey(Alumno)
+    asignatura = models.ForeignKey(Asignatura)
+
+class Solicitud(models.Model):
+    tipoSolicitud = models.CharField(max_length=100)
+
+    #Relaciones
+    asignatura = models.ForeignKey(Asignatura)
+    alumno = models.ForeignKey(Alumno)
